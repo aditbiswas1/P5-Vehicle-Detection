fe84daa3d7d12c1c6e4a4ecee74a22dd738db3d6
Adit Biswas <adit.biswas@grofers.com>
2017-02-25 19:25:04 +0530

    donwload dataset.sh

---
+++ download_dataset.sh
@@ -0,0 +1,4 @@
+wget https://s3.amazonaws.com/udacity-sdc/Vehicle_Tracking/non-vehicles.zip
+wget https://s3.amazonaws.com/udacity-sdc/Vehicle_Tracking/vehicles.zip
+unzip non-vehicles.zip
+unzip vehicles.zip
